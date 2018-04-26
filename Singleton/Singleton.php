4174195caf7a8e7d7bd523232b0a51bcbec94f97
patrykschaffner <dupadupa2>
2018-04-26 12:01:51 +0200

    add singleton design pattern

---
+++ Singleton/Singleton.php
@@ -0,0 +1,44 @@
+<?php
+
+class Singleton
+{
+    private static $instance;
+    private $test;
+
+    private function __construct()
+    {
+
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Singleton();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @param mixed $test
+     */
+    public function setTest($test): void
+    {
+        $this->test = $test;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTest()
+    {
+        return $this->test;
+    }
+}
+
+$instance1 = Singleton::getInstance();
+$instance1->setTest("1");
+echo $instance1->getTest();
+$instance2 = Singleton::getInstance();
+echo $instance1->getTest();
+
