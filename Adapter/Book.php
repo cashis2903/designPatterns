e0f716077cdf5432808eceb4173cac9e47874df3
patrykschaffner <dupadupa2>
2018-07-07 15:18:31 +0200

    Add simple adapter design pattern

---
+++ Adapter/Book.php
@@ -0,0 +1,50 @@
+<?php
+
+class Book {
+
+
+    private $title;
+    private $author;
+
+    public function __construct($title, $author)
+    {
+        $this->title = $title;
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+
+
+
+}
---
+++ Adapter/BookAdapter.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'Book.php';
+
+class BookAdapter
+{
+
+    private $book;
+
+    public function __construct(Book $book)
+    {
+        $this->book = $book;
+    }
+
+    public function getTitleAndAuthor()
+    {
+        return $this->book->getTitle() . ' - '. $this->book->getAuthor();
+    }
+
+}
+
+$book = new Book('Title', 'Author');
+$adapter = new BookAdapter($book);
+echo $adapter->getTitleAndAuthor();
+
+
